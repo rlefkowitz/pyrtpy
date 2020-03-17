43b6f4b5f1195518ad24981b448394066de89e67
Ryan Lefkowitz <rml258@cornell.edu>
2020-03-17 13:14:27 -0400

    setup.py Ready

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-rlefkowitz",
+    name="pyrtpy",
     version="0.0.1",
     author="Ryan Lefkowitz",
     author_email="rml258@cornell.edu",
